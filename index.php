3dc51ea337b7f216789ff352a1d409b7820675e1
edwin condori <ovidio43@gmail.com>
2014-03-11 09:29:37 -0400

    add files

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Mark & Associates</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="js/bxslider/jquery.bxslider.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <header id="header">
+            <div class="wrapper">
+                <a href="index.php" class="logo"><img src="img/logo.png"> </a>
+                <div class="head-caption">
+                    1488 Chain Bridge RD., STE. 302 Mclean, VA 22101  
+                    Tel. 703 790 9700   -   301840 9700            
+                </div>
+            </div>
+        </header>
+        <section id="main-content">
+            <div class="wrap-bxslider">
+                <div class="wrapper">
+                    <ul class="bxslider">
+                        <li>
+                            <div class="wrap-slider">
+                                <div class="img-slide col-md-5">
+                                    <img src="img/slide-1.png">
+                                </div>
+                                <div class="caption-slide col-md-7">
+                                    <h1>Conozca sus Derechos de Inmigracion</h1>
+                                    <ul>
+                                        <li>Defensa de reportacion</li>
+                                        <li>Permisos de trabajo</li>
+                                        <li>Peticiones de Familia</li>
+                                        <li>Legalizacion</li>
+                                        <li>Ciudadania</li>
+                                        <li>Carta de trabajo</li>
+                                    </ul>                                    
+                                    <a href="" class="btb-more">Consultenos Ahora</a>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="wrap-slider">
+                                <div class="img-slide col-md-5">
+                                    <img src="img/slide-1.png">
+                                </div>
+                                <div class="caption-slide col-md-7">
+                                    <h1>Derecho Familiar</h1>
+                                    <ul>
+                                        <li>Divorcio</li>
+                                        <li>Adopcion</li>
+                                        <li>Demandas Civiles</li>
+                                        <li>Cambio de nombre</li>
+                                        <li>Herencias</li>
+                                    </ul>                                    
+                                    <a href="" class="btb-more">Consultenos Ahora</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="wrap-slider">
+                                <div class="img-slide col-md-5">
+                                    <img src="img/slide-1.png">
+                                </div>
+                                <div class="caption-slide col-md-7">
+                                    <h1>Defensa Criminal</h1>
+                                    <ul>
+                                        <li>Drogas</li>
+                                        <li>Delitos Graves o leves</li>
+                                        <li>Delitos cometidos por menor de edad</li>
+                                    </ul>                                    
+                                    <a href="" class="btb-more">Consultenos Ahora</a>
+                                </div>
+                            </div>
+                        </li>    
+                        <li>
+                            <div class="wrap-slider">
+                                <div class="img-slide col-md-5">
+                                    <img src="img/slide-1.png">
+                                </div>
+                                <div class="caption-slide col-md-7">
+                                    <h1>infracciones de Transito</h1>
+                                    <p>DWI, Licencia Suspendida</p>
+                                    <a href="" class="btb-more">Consultenos Ahora</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="wrap-slider">
+                                <div class="img-slide col-md-5">
+                                    <img src="img/slide-1.png">
+                                </div>
+                                <div class="caption-slide col-md-7">
+                                    <h1>Otros Servicios</h1>
+                                    <ul>
+                                        <li>Accidentes de transito</li>
+                                        <li>Notarizaciones</li>
+                                        <li>Accidentes de auto</li>
+                                        <li>Traducciones</li>
+                                    </ul>
+                                    <a href="" class="btb-more">Consultenos Ahora</a>
+                                </div>
+                            </div>
+                        </li>                                                                                                                    
+                    </ul>
+                </div>
+            </div>
+            <div class="main-caption">
+                <div class="wrapper">
+                    <div class="caption">“Mas que abogados, somos los amigos que estan dispuestos a defender sus necesidades legales y las de su familia y amigos”</div>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="descript-content">
+                    <div class="col-md-6 img-descript"><img src="img/lawyer.jpg"></div>
+                    <div class="col-md-6 caption-descript">
+                        <h2>Atencion Personal y Eficaz</h2>
+                        <p>Nuestros Precios son razonables, y hablamos español, para mejor atencion ustedes se pueden comunicar por los siguientes datos.<br><br>
+
+                        Telefonos: 7037909700 - 3018409700<br>
+                        Direccion: 1487 Chain Bridge RD., STE. 302 Mclean, VA 22101<br>
+                        Fax: 7037900002<br>
+                        E-mail: <a href="mailto:mslaw1236@aol.com">mslaw1236@aol.com</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer id="footer">
+            <div class="wrapper">
+                <a href="index.php" class="logo-footer"><img src="img/logo.png"></a>
+                <p>Derechos reservados  2014 Marks & Associates</p>
+            </div>
+        </footer>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+        <script src="js/bxslider/jquery.bxslider.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
